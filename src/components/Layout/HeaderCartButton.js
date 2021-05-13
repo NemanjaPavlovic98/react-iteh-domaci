8e0b84e216d3cff8ff2c96bed8ddce429b91bfbf
NemanjaPavlovic <pavlovic.nemanja.1998@gmail.com>
2021-05-13 20:21:52 +0200

    implementing cart modal and functionalities

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,35 +1,35 @@
 import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
-// import CartContext from '../../store/cart-context';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
-//   const cartCtx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
 
-//  const { items } = cartCtx;
+ const { items } = cartCtx;
 
-//   const numberOfCartItems = items.reduce((curNumber, item) => {
-//     return curNumber + item.amount;
-//   }, 0);
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
-//   useEffect(() => {
-//     if (items.length === 0) {
-//       return;
-//     }
-//     setBtnIsHighlighted(true);
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
 
-//     const timer = setTimeout(() => {
-//       setBtnIsHighlighted(false);
-//     }, 300);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
 
-//     return () => {
-//       clearTimeout(timer);
-//     };
-//   }, [items]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick}>
@@ -37,7 +37,7 @@ const HeaderCartButton = (props) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      {/* <span className={classes.badge}>{numberOfCartItems}</span> */}
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
