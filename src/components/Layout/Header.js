a1bb3250492348b4c23ac2459e6a8f99227ee73c
NemanjaPavlovic <pavlovic.nemanja.1998@gmail.com>
2021-05-13 18:40:43 +0200

    setting up meal component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../Assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
@@ -9,7 +9,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ItehMeals</h1>
-        {/* <HeaderCartButton onClick={props.onShowCart} /> */}
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table with food!' />
